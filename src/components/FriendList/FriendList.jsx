f1b26f6a8b9d24b7e0990dba2115a8b1a5cbeeee
Maksym-Feshchenko <maksym.fd@gmai.com>
2023-05-11 00:43:25 +0300

    FriendListItem component

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,14 +2,27 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import css from './FriendList.module.css';
 
-const FriendList = ({ friends }) => (
-  <ul className={css.friend_list}>
-    {friends.map(friend => (
-    <li className={css.item} key={friend.id}>
-    <span className={friend.isOnline ? css.statusOnline: css.statusOfline}></span>
+const FriendListItem = ({ friend }) => (
+  <li className={css.item} key={friend.id}>
+    <span className={friend.isOnline ? css.statusOnline : css.statusOfline}></span>
     <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
     <p className={css.name}>{friend.name}</p>
   </li>
+);
+
+FriendListItem.propTypes = {
+  friend: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  }).isRequired,
+};
+
+const FriendList = ({ friends }) => (
+  <ul className={css.friend_list}>
+    {friends.map(friend => (
+    <FriendListItem friend={friend} />
     ))}
   </ul>
 );
